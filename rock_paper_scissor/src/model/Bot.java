11c4257ad9e9c27a4d88e97ecbce0a8cf93c7bea
diggo16 <daniell_94@hotmail.se>
2014-11-14 14:12:14 +0100

    GREEN

--- rock_paper_scissor/src/model/Bot.java
+++ rock_paper_scissor/src/model/Bot.java
@@ -1,12 +1,32 @@
 package model;
 
+import java.util.Random;
+
 public class Bot {
-	
+	private String choice;
 	
 	public void setChoice() {
-		
+		choice = generateChoice();
 	}
 	public String getChoice() {
+		return choice;
 	}
-
+	/*
+	 * generate an option
+	 */
+	private String generateChoice() {
+		Random random = new Random();
+		int randomNumber = random.nextInt(3);
+		String option = "";
+		if(randomNumber == 0) {
+			option = "rock";
+		}
+		if(randomNumber == 1) {
+			option = "paper";
+		}
+		if(randomNumber == 2) {
+			option = "scissor";
+		}
+		return option;
+	}
 }
