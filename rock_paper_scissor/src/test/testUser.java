7d8a9a23a5b834460dfa68972113d9e0e9cf58ae
diggo16 <daniell_94@hotmail.se>
2014-11-14 13:57:43 +0100

    RED

---
+++ rock_paper_scissor/src/test/testUser.java
@@ -0,0 +1,43 @@
+package test;
+
+import static org.junit.Assert.*;
+import model.User;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class testUser {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testSetAndGetChoice() {
+		User user = new User();
+		String choice = "rock";
+		user.setChoice(choice);
+		assertEquals(choice,user.getChoice());
+		
+	}
+
+}
