d1ceea4a23dd2abea8198f632c67c196f80bd703
diggo16 <daniell_94@hotmail.se>
2014-11-16 16:30:45 +0100

    RED

---
+++ rock_paper_scissor/src/test/TestConsole.java
@@ -0,0 +1,48 @@
+package test;
+
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import view.Console;
+
+public class TestConsole {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAskForInputOption() {
+		Console consoleStub = Mockito.mock(Console.class);
+		Mockito.when(consoleStub.askForInputOption()).thenReturn("");
+		
+		String input = consoleStub.askForInputOption();
+		if(input.equals("rock") != true) {
+			fail();
+		}
+	}
+	//@Test
+	public void testShowWinner() {
+		Console console = new Console();
+		console.showWinner("Winner");
+	}
+}
