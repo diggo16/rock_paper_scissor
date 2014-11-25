1db084c08f7fc9b691f504908ba6bc48bb6f596c
diggo16 <daniell_94@hotmail.se>
2014-11-25 13:15:05 +0100

    REFACTOR

--- rock_paper_scissor/src/test/TestInformation.java
+++ rock_paper_scissor/src/test/TestInformation.java
@@ -20,7 +20,8 @@ import org.junit.Test;
 public class TestInformation {
 	
 	private Information info;
-
+	private int user1Wins, user2Wins, user1Losses, user2Losses;
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -32,6 +33,11 @@ public class TestInformation {
 	@Before
 	public void setUp() throws Exception {
 		info = new Information("info.txt");
+		user1Wins = 3;
+		user1Losses = 1;
+		user2Wins = 2;
+		user2Losses = 4;
+		
 	}
 	@After
 	public void tearDown() throws Exception {
@@ -45,16 +51,16 @@ public class TestInformation {
 		List<User> users = new ArrayList<User>();
 		//user 1
 		User user1 = new User("Name1");
-		for(int i = 0; i < 3; i++) {
+		for(int i = 0; i < user1Wins; i++) {
 			user1.gameResult("won");
 		}
 		user1.gameResult("lost");
 		//user 2
 		User user2 = new User("Name2");
-		for(int i = 0; i < 2; i++) {
+		for(int i = 0; i < user2Wins; i++) {
 			user2.gameResult("won");
 		}
-		for(int i = 0; i < 4; i++) {
+		for(int i = 0; i < user2Losses; i++) {
 		user2.gameResult("lost");
 		}
 		
@@ -79,14 +85,14 @@ public class TestInformation {
 		User user1 = info.getUser("Name1");
 		assertEquals("Name1",user1.getName());
 		Statistics userStats1 = user1.getStatistics();
-		assertEquals(3,userStats1.getWins());
-		assertEquals(1,userStats1.getLosses());
+		assertEquals(user1Wins,userStats1.getWins());
+		assertEquals(user1Losses,userStats1.getLosses());
 		//user 2
 		User user2 = info.getUser("Name2");
 		assertEquals("Name2",user2.getName());
 		Statistics userStats2 = user2.getStatistics();
-		assertEquals(2,userStats2.getWins());
-		assertEquals(4,userStats2.getLosses());
+		assertEquals(user2Wins,userStats2.getWins());
+		assertEquals(user2Losses,userStats2.getLosses());
 	}
 
 }
